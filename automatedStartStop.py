4a6d0c94e6ac82540bcc774b6168cd6a660b8817
Phillip Passmore <phillipspassmore@gmail.com>
2023-06-16 17:06:50 +0100

    class structure
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- automatedStartStop.py
+++ automatedStartStop.py
@@ -6,44 +6,65 @@ from astral import LocationInfo
 from astral.sun import sun
 import pytz
 
-
-currentDate = datetime.date.today()
-
-print(currentDate)
-
-config_file = configparser.ConfigParser()
-
-# ADD SECTION
-config_file.add_section("YouTube LiveStream")
-
-# ADD SETTINGS TO SECTION
-config_file.set("YouTube LiveStream", "date", f"{currentDate}")
-config_file.set("YouTube LiveStream", "Started", "0")
-config_file.set("YouTube LiveStream", "Ended","0")
-
-# SAVE CONFIG FILE
-with open(r"config.ini", 'w') as configfileObj:
-    config_file.write(configfileObj)
-    configfileObj.flush()
-    configfileObj.close()
+class AutomatedStartStop:
     
-# PRINT FILE CONTENT
+    def __init__(self) -> None:
+        currentDate = datetime.date.today()
+        currentTime = datetime.datetime.now().strftime("%H:%M:%S")
+        config_file = configparser.ConfigParser()
+        sunrise, sunset = AutomatedStartStop.sun_data(self)
+        print(currentTime)
+        print(sunrise)
+        print(sunset)
+        return 
+    
+    def save_config(self, config_file):
+        with open(r"config.ini", 'w') as configfileObj:
+            config_file.write(configfileObj)
+            configfileObj.flush()
+            configfileObj.close()
+        
+    def sun_data(self):
+        city = LocationInfo("Falmouth", "England", "Europe/London", 50.15, -5.066)
+        timezone = pytz.timezone(city.timezone)
+        s = sun(city.observer)
+        sunrise = s["sunrise"].astimezone(timezone).strftime("%H:%M:%S")
+        sunset = s["sunset"].astimezone(timezone).strftime("%H:%M:%S")
+        return sunrise, sunset
+    
+
+    '''
+
+    # ADD SECTION
+    config_file.add_section("YouTube LiveStream")
+
+    # ADD SETTINGS TO SECTION
+    config_file.set("YouTube LiveStream", "date", f"{currentDate}")
+    config_file.set("YouTube LiveStream", "Started", "0")
+    config_file.set("YouTube LiveStream", "Ended","0")
+
+    # SAVE CONFIG FILE
+    
+        
+    # PRINT FILE CONTENT
 
 
-date = config_file.get('YouTube LiveStream', 'date')
+    date = config_file.get('YouTube LiveStream', 'date')
 
 
-print (date)
-date = datetime.date(2023, 4, 22)
-city = LocationInfo("Falmouth", "England", "Europe/London", 50.15, -5.066)
-timezone = pytz.timezone(city.timezone)
-s = sun(city.observer)
+    print (date)
+    #date = datetime.date(2023, 4, 22)
+    city = LocationInfo("Falmouth", "England", "Europe/London", 50.15, -5.066)
+    timezone = pytz.timezone(city.timezone)
+    s = sun(city.observer)
 
 
-print(
-    f'Dawn:    {s["dawn"].astimezone(timezone)}\n'
-    f'Sunrise: {s["sunrise"].astimezone(timezone)}\n'
-    f'Noon:    {s["noon"].astimezone(timezone)}\n'
-    f'Sunset:  {s["sunset"].astimezone(timezone)}\n'
-    f'Dusk:    {s["dusk"].astimezone(timezone)}\n'
-)
+    print(
+        f'Dawn:    {s["dawn"].astimezone(timezone)}\n'
+        f'Sunrise: {s["sunrise"].astimezone(timezone)}\n'
+        f'Noon:    {s["noon"].astimezone(timezone)}\n'
+        f'Sunset:  {s["sunset"].astimezone(timezone)}\n'
+        f'Dusk:    {s["dusk"].astimezone(timezone)}\n'
+    )
+    '''
+AutomatedStartStop()
