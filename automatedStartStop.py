f6133fdfd47e6c0ea75daea32bd10193d630c427
Dr-Passmore <phillipspassmore@gmail.com>
2023-07-13 17:59:39 +0100

    Create config function added

--- automatedStartStop.py
+++ automatedStartStop.py
@@ -5,6 +5,8 @@ import configparser
 from astral import LocationInfo
 from astral.sun import sun
 import pytz
+import logging
+import os
 
 class AutomatedStartStop:
     
@@ -13,11 +15,31 @@ class AutomatedStartStop:
         currentTime = datetime.datetime.now().strftime("%H:%M:%S")
         config_file = configparser.ConfigParser()
         sunrise, sunset = AutomatedStartStop.sun_data(self)
-        print(currentTime)
+        if os.path.exists("config.ini"):
+            #update config file if time is different
+            print("config exists")
+        else:
+            AutomatedStartStop.create_config(self, config_file, currentDate, sunrise, sunset)    
+        storedDate = config_file.get("YouTube LiveStream", "date")
+        print(storedDate)
         print(sunrise)
         print(sunset)
         return 
     
+    def create_config(self, config_file, currentDate, sunrise, sunset):
+        print(sunrise)
+        # ADD SECTION
+        config_file.add_section("YouTube LiveStream")
+
+        Youtube_livetream_code = input("Please enter required code")
+        # ADD SETTINGS TO SECTION
+        config_file.set("YouTube LiveStream", "date", str(currentDate))
+        config_file.set("YouTube LiveStream", "Started", "0")
+        config_file.set("YouTube LiveStream", "Ended","0")
+        config_file.set("YouTube LiveStream", "Code", Youtube_livetream_code)
+
+        self.save_config(config_file)
+
     def save_config(self, config_file):
         with open(r"config.ini", 'w') as configfileObj:
             config_file.write(configfileObj)
@@ -66,5 +88,15 @@ class AutomatedStartStop:
         f'Sunset:  {s["sunset"].astimezone(timezone)}\n'
         f'Dusk:    {s["dusk"].astimezone(timezone)}\n'
     )
+
+    
     '''
+# Logging
+logging.basicConfig(filename='BirdFeederLiveStream.log', 
+                    filemode='a', 
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
 AutomatedStartStop()
+
